4d2951e18410aa54502f8cc93130a9b3886ae878
jagosile <jagosile@gmail.com>
2020-10-15 12:19:40 +0200

    Create TweetsController.cs

---
+++ TwitterAPI/Controllers/TweetsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Application;
+using Application.DTO;
+using Application.DTO.Account;
+using Application.Services;
+using Domain.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TwitterAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TweetsController : ControllerBase
+    {
+
+        private readonly ILogger<TweetsController> _logger;
+        private readonly ITwitterService _twitterService;
+
+
+
+        public readonly UserManager<IdentityUser> _userManager;
+        public readonly SignInManager<IdentityUser> _signInManager;
+
+        public TweetsController(ILogger<TweetsController> logger, ITwitterService twitterService, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _logger = logger;
+            _twitterService = twitterService;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet("userId}", Name = "delete")]
+        public async Task<ActionResult> Subscribe(string userId)
+        {
+            await Task.Delay(100);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IdentityResult>> Create([FromBody] CreateUserRequest createUserRequest)
+        {
+            await Task.Delay(100);
+            return Ok();
+        }
+
+        [HttpGet("{tweetId}", Name = "delete")]
+        public async Task<ActionResult<IdentityResult>> Delete(int tweetId)
+        {
+            await Task.Delay(100);
+            return Ok();
+        }
+
+
+
+
+    }
+
+
+}
